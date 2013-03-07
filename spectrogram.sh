67b2197210c31431f993984dfcbac4ab3979501f
root <root@raspi.local>
2013-03-07 21:25:57 +0100

    Add shell scripts for spectrogram and spectrum visualisation

---
+++ spectrogram.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# Runs the spectrogram audio visualisation
+
+cd bannervis
+./spectrogram /dev/shm/squeezelite-b8:27:eb:f5:ed:87 30
+
---
+++ spectrum.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# Runs the spectrum audio visualisation
+
+cd bannervis
+./spectrum /dev/shm/squeezelite-b8:27:eb:f5:ed:87 30
+
